6cb6470b904e0d5bf98c778a9b6bb3bc1048e17e
kimachuk88 <kimachuk88@gmail.com>
2018-07-20 11:25:55 +0300

    Final

--- ChronometerTest/Tools/TestRunner.cs
+++ ChronometerTest/Tools/TestRunner.cs
@@ -1,14 +1,6 @@
 ﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using NUnit.Framework;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Support.UI;
-using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Android;
 using OpenQA.Selenium.Appium.Enums;
@@ -16,24 +8,38 @@ using OpenQA.Selenium.Remote;
 
 namespace ChronometerTest.Tools
 {
-    [TestFixture]
-    public abstract class TestRunner
-    {
-        
 
+    [TestFixture]
+    public class TestRunner
+    {
+        public AppiumDriver<AndroidElement> driver;
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        [SetUp]
         public void TestSetup() {
-            AppiumDriver<AndroidElement> driver;
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.SetCapability(MobileCapabilityType.App, @"C:\Users\HP DV2 - 1030us\AndroidStudioProjects\App\ApiDemos-debug.apk");
-            capabilities.SetCapability(MobileCapabilityType.DeviceName, "j53gxx");
-            capabilities.SetCapability(MobileCapabilityType.Udid, "33742dc5");
-            capabilities.SetCapability(MobileCapabilityType.PlatformVersion, "6.0.1");
+
+
+            capabilities.SetCapability(MobileCapabilityType.App, @"D:\ApiDemos-debug.apk");
+            capabilities.SetCapability(MobileCapabilityType.DeviceName, "emulator-5554");
+            capabilities.SetCapability(MobileCapabilityType.Udid, "emulator-5554");
+            capabilities.SetCapability(MobileCapabilityType.PlatformVersion, "6.0.0");
             capabilities.SetCapability(MobileCapabilityType.PlatformName, "Android");
             capabilities.SetCapability(MobileCapabilityType.FullReset, "false");
 
             driver = new AndroidDriver<AndroidElement>(new Uri("http://127.0.0.1:4723/wd/hub"), capabilities);
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(30));
+
+            //Locate testing app
+            driver.Swipe(0, 0, 0, 300, 0);
+            driver.FindElement(By.Id("Views")).Click();
+            driver.FindElement(By.Id("Chronometer")).Click();
         }
+        [TearDown]
+        public void TestTearDown()
+        {
+            driver.CloseApp();
+        } 
+
 
 
     }
--- ChronometerTest/UnitTest1.cs
+++ ChronometerTest/UnitTest1.cs
@@ -1,19 +1,8 @@
-﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿
 using System.Threading;
-using System.Threading.Tasks;
 using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Support.UI;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Appium;
-using OpenQA.Selenium.Appium.Android;
-using OpenQA.Selenium.Appium.Enums;
-using OpenQA.Selenium.Remote;
 using ChronometerTest.Tools;
+using OpenQA.Selenium;
 
 namespace ChronometerTest
 {
@@ -21,37 +10,46 @@ namespace ChronometerTest
     [TestFixture]
     public class UnitTest1 : TestRunner
     {
-        AppiumDriver<AndroidElement> driver;
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+        
 
         [Test]
         public void StartButtonTest()
         {
-            TestSetup();
-            driver.FindElement(By.XPath("//android.widget.TextView[@content-desc='Views']")).Click();
-            driver.FindElement(By.XPath("//android.widget.TextView[@content-desc='Chronometer']")).Click();
-
-            //Verify "Start" buttton affecting chronometer
+            Thread.Sleep(5000);
+            
             //Read chronometer before start
             string start = driver.FindElement(By.Id("io.appium.android.apis:id/chronometer")).Text;
+            
+
+            //Click Start
             driver.FindElement(By.Id("io.appium.android.apis:id/start")).Click();
             Thread.Sleep(2000);
+
+            //Click stop chronometer
+            driver.FindElement(By.Id("io.appium.android.apis:id/stop")).Click();
+
+            //Read chronometer 5 sec after start
             string finish = driver.FindElement(By.Id("io.appium.android.apis:id/chronometer")).Text;
+
+            //Reset chronometer
+            driver.FindElement(By.Id("io.appium.android.apis:id/reset")).Click();
 
             //Verify whether chronometer was working
             Assert.AreNotEqual(start, finish);
-
+            Thread.Sleep(5000);
+            
             //Exit app
-            driver.CloseApp();
+
         }
 
         [Test]
         public void StopButtonTest()
         {
-            TestSetup();
-
+            Thread.Sleep(5000);
             //Read chronometer before start and click Start
             string start = driver.FindElement(By.Id("io.appium.android.apis:id/chronometer")).Text;
+            
+            //Click Start
             driver.FindElement(By.Id("io.appium.android.apis:id/start")).Click();
 
             //2 sec wait
@@ -64,15 +62,24 @@ namespace ChronometerTest
             //Verify whether chronometer was working
             Assert.AreNotEqual(start, finish);
 
+            //5 seconds wait and read chronometer to check if chronometer is stopped
+            Thread.Sleep(2000);
+            string delayed = driver.FindElement(By.Id("io.appium.android.apis:id/chronometer")).Text;
+
+            //Reset chronometer
+            driver.FindElement(By.Id("io.appium.android.apis:id/reset")).Click();
+
+            //Compare chronometer reading after stop and 5 seconds later
+            Assert.AreEqual(finish, delayed);
+            
             //Exit app
-            driver.CloseApp();
+
         }
 
         [Test]
         public void ResetButtonTest()
         {
-            TestSetup();
-            
+            Thread.Sleep(5000);
             //Read chronometer before start and click Start
             string start = driver.FindElement(By.Id("io.appium.android.apis:id/chronometer")).Text;
             driver.FindElement(By.Id("io.appium.android.apis:id/start")).Click();
@@ -91,55 +98,57 @@ namespace ChronometerTest
             Assert.AreEqual(start, afterReset);
 
             //Exit app
-            driver.CloseApp();
+            
         }
         [Test]
         public void SetFormatButtonTest()
         {
-            TestSetup();
-
-            ////Read chronometer before start 
-            //string start = driver.FindElement(By.Id("io.appium.android.apis:id/chronometer")).Text;
-
-            //Click Set Format String button
-
-
+            Thread.Sleep(5000);
+            
             //Click Start
             driver.FindElement(By.Id("io.appium.android.apis:id/start")).Click();
+            
+
+            //Click Set Format String button
+            driver.FindElement(By.Id("Set format string")).Click();
+            
+
+            //Click stop 
+            driver.FindElement(By.Id("io.appium.android.apis:id/stop")).Click();
+            Thread.Sleep(5000);
 
             //Read chronometer
             string afterSetFormat = driver.FindElement(By.Id("io.appium.android.apis:id/chronometer")).Text;
 
             //Check whether chronometer contains text SetFormat String
-            Assert.IsTrue(afterSetFormat.Contains("Set Format String"));
+            StringAssert.Contains("Formatted time",afterSetFormat,"Formatted time is not displayed");
 
             //Exit app
-            driver.CloseApp();
+            
             
         }
 
         [Test]
         public void ClearFormatButtonTest()
         {
-            TestSetup();
-
-            //Click Set Format String button
-
-
+            Thread.Sleep(5000);
             //Click Start
             driver.FindElement(By.Id("io.appium.android.apis:id/start")).Click();
 
             //Click Clear Format String Button
+            driver.FindElement(By.Id("Clear format string")).Click();
 
+            //Click stop 
+            driver.FindElement(By.Id("io.appium.android.apis:id/stop")).Click();
 
             //Read chronometer
             string afterClearFormat = driver.FindElement(By.Id("io.appium.android.apis:id/chronometer")).Text;
 
-            //Count whether chronometer contains only 4 symbols 
-            Assert.IsTrue(afterClearFormat.Length == 5);
+            //Count whether chronometer contains only 5 symbols 
+            Assert.IsTrue(afterClearFormat.Length == 5,"00:00 is not displayed");
 
             //Exit app
-            driver.CloseApp();
+            
 
         }
 
